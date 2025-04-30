1fde16d7347d286b1c84b572802aa28e8a3b96e1
StandingAsh <tjswopark0123@gmail.com>
2025-04-30 13:10:52 +0900

    Meta annotation scanning updated

--- src/main/java/com/standingash/jettystick/core/scanners/ComponentScanner.java
+++ src/main/java/com/standingash/jettystick/core/scanners/ComponentScanner.java
@@ -3,6 +3,7 @@ package com.standingash.jettystick.core.scanners;
 import com.standingash.jettystick.core.annotations.Component;
 import org.reflections.Reflections;
 
+import java.lang.annotation.Annotation;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -11,8 +12,23 @@ public class ComponentScanner {
     // filters annotations themselves. e.g. @View, @Service
     public static Set<Class<?>> scan(String basePackage) {
         Reflections reflections = new Reflections(basePackage);
-        return reflections.getTypesAnnotatedWith(Component.class)
-                .stream().filter(component -> !component.isAnnotation())
+        return reflections.getTypesAnnotatedWith(Component.class).stream()
+                .filter(clazz -> !clazz.isAnnotation())
+                .filter(ComponentScanner::isComponent)
                 .collect(Collectors.toSet());
+    }
+
+    private static boolean isComponent(Class<?> clazz) {
+
+        // directly annotated with @Component
+        if (clazz.isAnnotationPresent(Component.class))
+            return true;
+
+        // checks for meta-annotation
+        for (Annotation annotation : clazz.getAnnotations())
+            if (annotation.annotationType().isAnnotationPresent(Component.class))
+                return true;
+
+        return false;
     }
 }
